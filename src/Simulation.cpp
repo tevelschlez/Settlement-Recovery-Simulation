a46e2599ac31295f057a54c1de83b5400baf4f4a
tevelschlez <tevel138@gmail.com>
2024-11-30 18:14:24 +0000

    fixed logic in the start function again

--- src/Simulation.cpp
+++ src/Simulation.cpp
@@ -27,15 +27,16 @@ class Simulation{
 
     public:
 
-        Simulation::Simulation(const string &configFilePath):isRunning(false),planCounter(0){}
+        Simulation::Simulation(const string &configFilePath):isRunning(false),planCounter(0){
+
+        }
 
         void Simulation::start(){// should print: "The simulation has started". should also start the loop of inputs from the user.
-        //all the initialziations from the configuration file should be added to the matching list field of this class
-
             open();//isRunning =true
             string command;
+            std::cout << "The simulation has started" << std::endl;
 
-<<<<<<< HEAD
+
             while (isRunning)
             {
                 //analyziation of the user input
@@ -43,95 +44,91 @@ class Simulation{
                 string command;
 
                 getline(std::cin, command);
-=======
-            getline(std::cin, command);
->>>>>>> origin/featuresimulation-class
+                BaseAction *action;
 
-            vector<string> arguments = Auxiliary::parseArguments(command);
+                vector<string> arguments = Auxiliary::parseArguments(command);
 
-            const string &actionType = arguments[0];
+                const string &actionType = arguments[0];
 
-            if(actionType=="Plan"){
-                const string &settlementName = arguments[1];
-                SelectionPolicy *selection;
+                if(actionType=="Plan"){
+                 const string &settlementName = arguments[1];
+                    SelectionPolicy *selection;
 
-                if(arguments[2]=="nve")
-                    selection = new NaiveSelection();
-                else if (arguments[2]=="bal")
-                    selection = new BalancedSelection(0,0,0);
-                else if (arguments[2]=="eco")
-                    selection = new EconomySelection();
-                else if (arguments[2]=="env")
-                    selection=new SustainabilitySelection();
+                    if(arguments[2]=="nve")
+                        selection = new NaiveSelection();
+                    else if (arguments[2]=="bal")
+                        selection = new BalancedSelection(0,0,0);
+                  else if (arguments[2]=="eco")
+                        selection = new EconomySelection();
+                    else if (arguments[2]=="env")
+                        selection=new SustainabilitySelection();
 
-                if(!isSettlementExists(settlementName))
-                    throw std::invalid_argument("the settlement does not exist");
-                else{
-                    planCounter++;
-                    plans.push_back(Plan(planCounter, getSettlement(settlementName), selection, facilitiesOptions));
-                }
-            }
+                    if(!isSettlementExists(settlementName))
+                        throw std::invalid_argument("the settlement does not exist");
+                 else{
+                     action = new AddPlan(settlementName, arguments[2]);
+                     //whats the add plan method should do? it soes not get any parameters
+                 }
+              }
 
-            if (actionType=="settlement"){
-                const string &settlementName = arguments[1];
-                SettlementType settlementType;
-                switch(stoi(arguments[2])){
+                if (actionType=="settlement"){
+                    const string &settlementName = arguments[1];
+                    SettlementType settlementType;
+                    switch(stoi(arguments[2])){
+                        case 0:
+                            settlementType = SettlementType::VILLAGE;
+                            break;
+                        case 1:
+                            settlementType = SettlementType::CITY;
+                            break;
+                        case 2:
+                            settlementType = SettlementType::METROPOLIS;
+                            break;
+                        default:
+                            throw std::invalid_argument("Unkown settlement type");
+                        }
+
+                        if(isSettlementExists(settlementName))
+                            throw std::invalid_argument("the settlement already exist - choose a uniqe name");
+                        else{
+                            action = new AddSettlement(settlementName,settlementType);
+                            action->act(*this);
+                        }
+                                }
+
+                if (actionType=="Facility"){
+                    const string &facilityName = arguments[1];
+                    FacilityCategory category;
+                    const int &price = stoi(arguments[3]);
+                    const int &lifeQuality_score = stoi(arguments[4]);
+                    const int &economy_score = stoi(arguments[5]);
+                    const int &enviroment_Score = stoi(arguments[6]);
+
+                    switch (stoi(arguments[2]))
+                    {
                     case 0:
-                        settlementType = SettlementType::VILLAGE;
+                        category = FacilityCategory::LIFE_QUALITY;
                         break;
                     case 1:
-                        settlementType = SettlementType::CITY;
+                        category = FacilityCategory::ECONOMY;
                         break;
                     case 2:
-                        settlementType = SettlementType::METROPOLIS;
+                        category = FacilityCategory::ENVIRONMENT;
                         break;
                     default:
-                        throw std::invalid_argument("Unkown settlement type");
+                        throw std::invalid_argument("Unkown facility category");
                     }
 
-                    if(isSettlementExists(settlementName))
-                        throw std::invalid_argument("the settlement already exist - choose a uniqe name");
-                    else
-                        settlements.push_back(new Settlement(settlementName, settlementType));
-            }
-
-            if (actionType=="Facility"){
-                const string &facilityName = arguments[1];
-                FacilityCategory category;
-                const string &price = arguments[3];
-                const string &lifeQuality_score = arguments[4];
-                const string &economy_score = arguments[5];
-                const string &enviroment_Score = arguments[6];
-
-                switch (stoi(arguments[2]))
-                {
-                case 0:
-                    category = FacilityCategory::LIFE_QUALITY;
-                    break;
-                case 1:
-                    category = FacilityCategory::ECONOMY;
-                    break;
-                case 2:
-                    category = FacilityCategory::ENVIRONMENT;
-                    break;
-                default:
-                    throw std::invalid_argument("Unkown facility category");
+                    if(isFacilityExists(facilityName))
+                        throw std::invalid_argument("facility already exist - choose a uniqe name");
+                    else{
+                        action = new AddFacility(facilityName, category, price, lifeQuality_score, economy_score, enviroment_Score);
+                        action->act(*this);
+                    }
                 }
-
-                if(isFacilityExists(facilityName))
-                    throw std::invalid_argument("facility already exist - choose a uniqe name");
-                else
-                    facilitiesOptions.push_back(FacilityType(facilityName,category,std::stoi(price),std::stoi(lifeQuality_score),std::stoi(economy_score),std::stoi(enviroment_Score)));
-            }
-
-            std::cout << "The simulation has started" << std::endl;
-
-            while (isRunning)
-            {
-                
-      
             }
         }
+
         void Simulation::addPlan(){
 
         }
